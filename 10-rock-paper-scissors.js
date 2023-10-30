6f6ad77a85e3265ea227285a4cf7fbf4fdb4c796
Firdous <126150589+DousyTech@users.noreply.github.com>
2023-10-30 12:04:22 +0200

    Add files via upload

---
+++ 10-rock-paper-scissors.js
@@ -0,0 +1,69 @@
+let score = (JSON.parse(localStorage.getItem('score'))) || {Wins:0,
+  Losses: 0,
+  Ties: 0,};
+
+  
+  updateScoreElement()
+
+// if (!score){score=
+//   { Wins:0,
+//     Losses: 0,
+//     Ties: 0,}};
+
+
+function playgame(playerMove){const computerMove = pickcomputermove();
+
+let result = '';
+
+if (playerMove=== 'scissors'){
+
+if (computerMove === 'Scissors'){result = 'Tie';}
+else if (computerMove ==='Rock'){result = 'You lose';}
+else if (computerMove === 'Paper'){result = 'You win';}}
+
+
+else if (playerMove=== 'paper'){
+
+if (computerMove === 'Paper'){result = 'Tie';}
+else if (computerMove ==='Scissors'){result = 'You lose';}
+else if (computerMove === 'Rock'){result = 'You win';}}
+
+else if (playerMove=== 'Rock'){
+
+if (computerMove === 'Rock'){result = 'Tie';}
+else if (computerMove ==='Paper'){result = 'You lose';}
+else if (computerMove === 'Scissors'){result = 'You win';}}
+
+if (result === 'You win'){score.Wins += 1;}
+else if (result === 'You lose'){score.Losses += 1;}
+else if (result === 'Tie'){score.Ties += 1;}
+
+localStorage.setItem('score',JSON.stringify(score));
+
+updateScoreElement();
+
+document.querySelector('.js-result')
+  .innerHTML= result;
+
+document.querySelector('.js-moves')
+  .innerHTML= `You
+<img src="images/${playerMove}-emoji.png" class="move-icon">
+<img src="images/${computerMove}-emoji.png " class="move-icon">
+Computer`
+} 
+
+function updateScoreElement (){document.querySelector('.js-score')
+  .innerHTML= `Wins: ${score.Wins}, Losses: ${score.Losses}, ties: ${score.Ties}`};
+
+function pickcomputermove (){const randomNumber = Math.random();
+
+let computerMove = '';
+
+if (randomNumber >= 0 && randomNumber < 1/3){ computerMove = 'Rock';}
+
+else if (randomNumber >= 1/3 && randomNumber <2/3){computerMove ='Paper';}
+
+else if (randomNumber >= 2/3 && randomNumber <1){ computerMove='Scissors'}
+
+return computerMove
+}
